7207eebed2b8e315ce58a20fffe49385932d6960
Hassnain-Idrees <hassnainidrees@gmail.com>
2025-01-24 17:08:37 +0500

    TODO LIST APP COMPLETETD

--- lib/todolist_App.dart
+++ lib/todolist_App.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'To-Do List App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
       home: TodoScreen(),
       debugShowCheckedModeBanner: false,
